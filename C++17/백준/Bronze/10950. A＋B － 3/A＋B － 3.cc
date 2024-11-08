5cde7efdb823499c90733e9c355657cf57b6a95f
minguencho <80305473+minguencho@users.noreply.github.com>
2024-11-08 11:28:24 +0900

    Update A＋B － 3.cc

--- C++17/백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
+++ C++17/백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -2,7 +2,7 @@
 
 int main() {
     int a,b,c;
-    scanf("%d", &a);  // 주사위 세 개의 눈 입력 받기
+    scanf("%d", &a);
     for (int i = 0; i < a; i++) {
         scanf("%d %d", &b, &c);
         printf("%d\n",b+c);
