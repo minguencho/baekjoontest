13fdf7ef342252d700c3b51b539c3f7ced0005cd
minguencho <80305473+minguencho@users.noreply.github.com>
2024-11-08 10:13:24 +0900

    [Bronze III] Title: 알람 시계, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2884. 알람 시계/알람 시계.cc
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int h, m;
+    scanf("%d %d",&h, &m);
+    if (m < 45) {
+        if (h - 1 < 0) {
+            printf("%d %d", 23 , 60 - (45 - m));
+        }
+        else {
+            printf("%d %d", h - 1, 60 - (45 - m));
+        }
+    }
+    else {
+        printf("%d %d", h , m -45);
+    }
+    return 0;
+}
