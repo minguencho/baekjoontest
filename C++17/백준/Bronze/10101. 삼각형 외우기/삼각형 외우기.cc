422e51f6cfd7db97b6aadfc721f9daadd7505f3e
minguencho <80305473+minguencho@users.noreply.github.com>
2024-12-27 14:18:29 +0900

    [Bronze IV] Title: 삼각형 외우기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10101. 삼각형 외우기/삼각형 외우기.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int angle1, angle2, angle3;
+    cin >> angle1 >> angle2 >> angle3;
+
+    int sum = angle1 + angle2 + angle3;
+
+    if (sum != 180) {
+        cout << "Error" << endl;
+    }
+    else if (angle1 == 60 && angle2 == 60 && angle3 == 60) {
+        cout << "Equilateral" << endl;
+    }
+    else if (angle1 == angle2 || angle2 == angle3 || angle1 == angle3) {
+        cout << "Isosceles" << endl;
+    }
+    else {
+        cout << "Scalene" << endl;
+    }
+
+    return 0;
+}
