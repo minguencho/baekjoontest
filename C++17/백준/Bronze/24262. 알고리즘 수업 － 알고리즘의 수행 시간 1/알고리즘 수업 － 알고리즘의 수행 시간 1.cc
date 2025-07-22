e352983af590f6a2b2d5456a70a2400e0f33599a
minguencho <80305473+minguencho@users.noreply.github.com>
2025-07-22 12:41:00 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    cout << 1 << endl; // 코드1의 실행 횟수
+    cout << 0 << endl; // 시간 복잡도 최고차항의 차수
+    return 0;
+}
